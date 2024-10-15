f20dd4fbb2060ebc47bacd29f0ac56eb1161af9a
MIRAS Romain <romain.miras@gmail.com>
2024-10-15 22:17:41 +0200

    Working QueueBroker

--- task4/src/messages/MessageQueue.java
+++ task4/src/messages/MessageQueue.java
@@ -1,32 +1,78 @@
 package messages;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+import event.Task;
+import ichannels.IChannel;
 import imessages.IMessageQueue;
 import imessages.Message;
 
 public class MessageQueue implements IMessageQueue {
+	
+	private IChannel channel;
+	private Queue<Message> messages;
+	private Listener listener;
+	private Task sendTask;
+	private MyListener myListener;
+
+	public MessageQueue(IChannel channel) {
+		this.channel = channel;
+		messages = new LinkedList();
+		sendTask = new Task("SendTask");
+		myListener = new MyListener();
+	}
 
 	@Override
 	public void setListener(Listener listener) {
-		// TODO Auto-generated method stub
-		
+		this.listener = listener;
+		myListener.setListener(listener);
 	}
 
 	@Override
 	public boolean send(Message message) {
-		// TODO Auto-generated method stub
-		return false;
+		if (channel.disconnected()) {
+			return false;
+		}
+
 	}
 
 	@Override
 	public void close() {
-		// TODO Auto-generated method stub
-		
+		channel.disconnect();
 	}
 
 	@Override
 	public boolean closed() {
-		// TODO Auto-generated method stub
-		return false;
+		return channel.disconnected();
+	}
+	
+	private class MyListener implements IChannel.Listener {
+		
+		private Listener listener;
+		
+		public MyListener() {
+			this.listener = null;
+		}
+		
+		void setListener(Listener listener) {
+            this.listener = listener;
+        }
+
+		@Override
+		public void readed(byte[] bytes) {
+			
+		}
+
+		@Override
+		public void disconnected() {
+			
+		}
+
+		@Override
+		public void wrote(int bytesWrote) {
+			
+		}
 	}
 
 }
--- task4/src/messages/QueueBroker.java
+++ task4/src/messages/QueueBroker.java
@@ -1,37 +1,72 @@
 package messages;
 
+import channels.Broker;
+import ichannels.IBroker;
+import ichannels.IChannel;
 import imessages.IQueueBroker;
 
 public class QueueBroker implements IQueueBroker {
 	
 	private String name;
+	private Broker broker;
 	
 	public QueueBroker(String string) {
-		// TODO Auto-generated constructor stub
+		this.name = string;
+		this.broker = new Broker(string);
 	}
 
 	@Override
 	public String name() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.name;
 	}
 
 	@Override
 	public boolean unbind(int port) {
-		// TODO Auto-generated method stub
-		return false;
+		return broker.unbind(port);
 	}
 
 	@Override
 	public boolean bind(int port, AcceptListener listener) {
-		// TODO Auto-generated method stub
-		return false;
+		MyAcceptListener acceptListener = new MyAcceptListener(listener);
+		return broker.bind(port, acceptListener);
 	}
 
 	@Override
 	public boolean connect(String name, int port, ConnectListener listener) {
-		// TODO Auto-generated method stub
-		return false;
+		MyConnectListener connectListener = new MyConnectListener(listener);
+        return broker.connect(name, port, connectListener);
+	}
+	
+	private class MyAcceptListener implements IBroker.AcceptListener {
+		private AcceptListener listener;
+
+		public MyAcceptListener(AcceptListener listener) {
+			this.listener = listener;
+		}
+
+		@Override
+		public void accepted(IChannel queue) {
+			listener.accepted(new MessageQueue(queue));
+		}
+	}
+	
+	private class MyConnectListener implements IBroker.ConnectListener {
+		
+		private ConnectListener listener;
+		
+		public MyConnectListener(ConnectListener listener) {
+			this.listener = listener;
+		}
+
+		@Override
+		public void connected(IChannel queue) {
+			listener.connected(new MessageQueue(queue));
+		}
+
+		@Override
+		public void refused() {
+			listener.refused();
+		}
 	}
 	
 }
